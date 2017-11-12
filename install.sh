adc0df3bd71469b4d78e7a93b035fa5f18a8d795
noam <noam@lifeonair.com>
2017-11-12 14:47:58 +0200

    more mature, but still not ready

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ sudo apt-get install -y ansible openssh-server git
 # after installing ansible:
 sudo ansible-galaxy install theNewFlesh.sublime
 sudo ansible-galaxy install henriklynggaard.clion
-sudo ansible-galaxy install theNewFlesh.sublime
+sudo ansible-galaxy install gantsign.gitkraken
 
 
-sudo printf "[local]\n127.0.0.1\n" >> /etc/ansible/hosts
+printf "[local]\n127.0.0.1\n" | sudo tee -a  /etc/ansible/hosts
 
-git clone git@github.com:noamcohen/my-ansible.git
-cd my-ansible
+#git clone git@github.com:noamcohen/my-ansible.git
+#cd my-ansible
 ansible-playbook basic.yml --ask-sudo-pass
 
